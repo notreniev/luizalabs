c491788cb2b5705759e259ae6212a74510511658
notreniev <everton.canez@gmail.com>
2021-03-13 17:52:16 -0300

    Remoção de usuários pela lista

--- src/app/aluno/aluno.page.ts
+++ src/app/aluno/aluno.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Aluno } from '../shared/models/aluno/aluno-model';
 import { Generico } from '../shared/models/aluno/generico-model';
+import { AlertService } from '../shared/providers/alert.service';
 import { DataService } from '../shared/providers/data.service';
 
 @Component({
@@ -14,10 +17,76 @@ export class AlunoPage implements OnInit {
 
   @ViewChild('search') search: string;
 
-  constructor(private dataProvider: DataService) {
+  constructor(
+    private dataProvider: DataService,
+    private alertService: AlertService,
+    private alertController: AlertController
+  ) {
     this.dataProvider.data$.subscribe(() => {
       this.alunos = this.dataProvider.data$.value
     })
+  }
+
+  /**
+ * Exibe alerta de remoção e
+ * remove caso afirmativo
+ * 
+ * @param aluno 
+ */
+  confirmaRemover = async (aluno: any) => {
+    const config = {
+      header: 'Confirmação!',
+      message: 'Deseja remover o aluno?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Canceled');
+          }
+        },
+        {
+          text: 'Remover',
+          handler: () => {
+            this.remover(aluno)
+          }
+        }
+      ]
+    }
+
+    await this.alertController
+      .create(config)
+      .then(res => res.present())
+      .catch(error => console.error(error))
+  }
+
+  /**
+   * Remove aluno
+   * 
+   * @param aluno 
+   */
+  remover = async (aluno) => {
+    if (!aluno) return
+
+    try {
+      if (aluno.id) {
+        await this.dataProvider.delete(`aluno/${aluno.id}`).toPromise()
+        this.alertService.success('Aluno removido com sucesso!')
+
+        setTimeout(() => {
+          this.getAlunos()
+        }, 1000)
+
+      }
+    } catch (error) {
+      this.alertService.error(`Erro ao remover aluno! ${error.message}`)
+    }
+  }
+
+
+  remove = async (aluno: Aluno) => {
+    console.log(aluno)
+    await this.dataProvider.delete(`aluno/${aluno.id}`).toPromise()
+    this.getAlunos()
   }
 
   getAlunos = async () => {
